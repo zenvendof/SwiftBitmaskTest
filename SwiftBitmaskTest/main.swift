636544400f17229e7e44d2189986bbb3a12e12bd
Tommy Cheung <tomicheung@gmail.com>
2015-05-06 10:13:42 +0800

    Initial Commit

---
+++ SwiftBitmaskTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBitmaskTest
+//
+//  Created by Tommy Cheung on 5/6/15.
+//  Copyright (c) 2015 zenvendof. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
